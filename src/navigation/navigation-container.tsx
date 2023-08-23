1408f9ed0b38f64c69afb27a4ca30c96b7024ab1
Mervin Narvaez <>
2023-08-23 12:08:53 +0800

    feat: add navigation ref

--- src/navigation/navigation-container.tsx
+++ src/navigation/navigation-container.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {NavigationContainer} from '@react-navigation/native'
 import RootStack from './stacks/root'
+import {useNavigationContainerRef} from '@react-navigation/native'
 
 export default () => {
+  const navigationRef = useNavigationContainerRef()
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <RootStack />
     </NavigationContainer>
   )
